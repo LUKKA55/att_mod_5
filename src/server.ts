7059f9ec93bbe87cf09d96fa18332036fba4ba40
Lucas dos Santos <luquinhaschely@gmail.com>
2023-04-11 23:00:36 -0300

    cors

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import express, { Request, Response } from 'express';
 import routers from './routers';
+import cors from 'cors';
 
 const server = express();
-server.use(express.json());
+server.use([express.json(), cors()]);
 server.use(routers);
 const port = process.env.PORT;
 server.listen(port, () => console.log(`server rodando`));
